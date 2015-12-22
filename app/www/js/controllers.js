f31327d1472c10ccdaebec65ecc4588074de93ad
Tania Chattopadhyay <tchatterjee5@gmail.com>
2015-12-22 12:21:34 +0800

    Add list of products

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -17,10 +17,25 @@ angular.module('app.controllers', [])
 })
    
 .controller('homeCtrl', function($scope) {
-
+	$scope.data = {};
+	$scope.productSearch = "";
+	$scope.productList = [];
+	  
+	$scope.saveProductSearch = function() {
+		if ($scope.data.product)
+	    {
+	      $scope.productSearch = $scope.data.product;
+	      $scope.productList.push({
+	      	first: $scope.productSearch
+	      });
+	    } 
+	    else 
+	    {
+	      alert("Please fill out the field before submitting!");
+	    }
+	}
 })
    
 .controller('myOrdersCtrl', function($scope) {
 
 })
-    
